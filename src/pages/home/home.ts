370a6424fb1c295c06e3c05755f8c65438cf824d
rbluthy <rbluthy@go.olemiss.edu>
2018-10-02 14:57:33 -0500

    Login alert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {AlertController} from 'ionic-angular';
+
+
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alerCtrl: AlertController) { }
 
+  doAlert() {
+    let alert = this.alerCtrl.create({
+      title: 'Login Alert!',
+      message: 'Not implemented yet!',
+      buttons: ['Ok']
+    });
+    alert.present()
   }
 
 }
