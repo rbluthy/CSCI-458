e6aaf2ed6c3050f00a19d4390a65f58d5f035d51
rbluthy <rbluthy@go.olemiss.edu>
2018-10-24 14:15:18 -0500

    Week buttons and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,22 @@ import { LoginPage} from '../pages/login/login';
 import { RegisterPage} from '../pages/register/register';
 
 import {W1Page} from '../pages/w1/w1';
+import {W2Page} from '../pages/w2/w2';
+import {W3Page} from '../pages/w3/w3';
+import {W4Page} from '../pages/w4/w4';
+import {W5Page} from '../pages/w5/w5';
+import {W6Page} from '../pages/w6/w6';
+import {W7Page} from '../pages/w7/w7';
+import {W8Page} from '../pages/w8/w8';
+import {W9Page} from '../pages/w9/w9';
+import {W10Page} from '../pages/w10/w10';
+import {W11Page} from '../pages/w11/w11';
+import {W12Page} from '../pages/w12/w12';
+import {W13Page} from '../pages/w13/w13';
+import {W14Page} from '../pages/w14/w14';
+import {W15Page} from '../pages/w15/w15';
+import {BowlsPage} from '../pages/bowls/bowls';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -32,7 +48,22 @@ import { RegisterPageModule } from '../pages/register/register.module';
     TeamsPage,
     TabsPage,
     LoginPage,
-    W1Page
+    W1Page,
+    W2Page,
+    W3Page,
+    W4Page,
+    W5Page,
+    W6Page,
+    W7Page,
+    W8Page,
+    W9Page,
+    W10Page,
+    W11Page,
+    W12Page,
+    W13Page,
+    W14Page,
+    W15Page,
+    BowlsPage
 
   ],
   imports: [
@@ -58,7 +89,22 @@ import { RegisterPageModule } from '../pages/register/register.module';
     TeamsPage,
     TabsPage,
     LoginPage,
-    W1Page
+    W1Page,
+    W2Page,
+    W3Page,
+    W4Page,
+    W5Page,
+    W6Page,
+    W7Page,
+    W8Page,
+    W9Page,
+    W10Page,
+    W11Page,
+    W12Page,
+    W13Page,
+    W14Page,
+    W15Page,
+    BowlsPage
 
   ],
   providers: [
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {W1Page} from '../w1/w1';
+import {W2Page} from '../w2/w2';
+import {W3Page} from '../w3/w3';
+import {W4Page} from '../w4/w4';
+import {W5Page} from '../w5/w5';
+import {W6Page} from '../w6/w6';
+import {W7Page} from '../w7/w7';
+import {W8Page} from '../w8/w8';
+import {W9Page} from '../w9/w9';
+import {W10Page} from '../w10/w10';
+import {W11Page} from '../w11/w11';
+import {W12Page} from '../w12/w12';
+import {W13Page} from '../w13/w13';
+import {W14Page} from '../w14/w14';
+import {W15Page} from '../w15/w15';
+import {BowlsPage} from '../bowls/bowls';
 
 @Component({
   selector: 'page-contact',
@@ -14,6 +29,51 @@ export class ContactPage {
 
   openW1Page(){
     this.navCtrl.push(W1Page);
+  }
+  openW2Page(){
+    this.navCtrl.push(W2Page);
+  }
+  openW3Page(){
+    this.navCtrl.push(W3Page);
+  }
+  openW4Page(){
+    this.navCtrl.push(W4Page);
+  }
+  openW5Page(){
+    this.navCtrl.push(W5Page);
+  }
+  openW6Page(){
+    this.navCtrl.push(W6Page);
+  }
+  openW7Page(){
+    this.navCtrl.push(W7Page);
+  }
+  openW8Page(){
+    this.navCtrl.push(W8Page);
+  }
+  openW9Page(){
+    this.navCtrl.push(W9Page);
+  }
+  openW10Page(){
+    this.navCtrl.push(W10Page);
+  }
+  openW11Page(){
+    this.navCtrl.push(W11Page);
+  }
+  openW12Page(){
+    this.navCtrl.push(W12Page);
+  }
+  openW13Page(){
+    this.navCtrl.push(W13Page);
+  }
+  openW14Page(){
+    this.navCtrl.push(W14Page);
+  }
+  openW15Page(){
+    this.navCtrl.push(W15Page);
+  }
+  openBowlsPage(){
+    this.navCtrl.push(BowlsPage);
   }
 
   itemSelected(item: string) {
