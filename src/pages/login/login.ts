5dd640a50cc5b368066988aead386a46bc934539
rbluthy <rbluthy@go.olemiss.edu>
2018-10-26 13:38:19 -0500

    Alerts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 
@@ -30,11 +30,19 @@ export class LoginPage {
 	@ViewChild('username') user;
 	@ViewChild('password') password;
 
-  constructor(private fire:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private fire:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  alert(message: string){
+    this.alertCtrl.create({
+      title: 'Hello!',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
   }
 
   
@@ -43,13 +51,13 @@ export class LoginPage {
     this.fire.auth.signInWithEmailAndPassword(this.user.value, this.password.value)
     .then( data => {
       console.log('got some data', this.fire.auth.currentUser);
-      
+      this.alert('You are logged in');
       this.navCtrl.setRoot( TabsPage );
       // user is logged in
     })
     .catch( error => {
       console.log('got an error', error);
-      
+      this.alert(error.message);
     })
   	console.log('Would sign in with ', this.user.value, this.password.value);
   }
